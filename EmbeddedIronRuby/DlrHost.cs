cc7ada72e9795b4ab630ba0cbb3d5eb141a2eff4
Ben Hall <Ben@BenHall.me.uk>
2010-03-21 12:12:07 +0000

    Example of embeddeding IronRuby into C#

---
+++ EmbeddedIronRuby/DlrHost.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+
+namespace EmbeddedIronRuby
+{
+    public class DlrHost
+    {
+        public void Run()
+        {
+            string line;
+            do
+            {
+                line = GetLine("C#");
+
+                if (line.Equals("ir"))
+                    EnterIronRuby();
+                else
+                    Console.WriteLine("You typed: " + line);
+
+            } while (line != "quit");
+        }
+
+        private void EnterIronRuby()
+        {
+            SetConsoleColour();
+
+            ScriptEngine engine = IronRuby.Ruby.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+
+            string line;
+            do
+            {
+                line = GetLine("ir");
+                ExecuteRuby(line, engine, scope);
+
+            } while (line != "quit");
+
+            ResetConsole();
+        }
+
+        private void ExecuteRuby(string line, ScriptEngine engine, ScriptScope scope)
+        {
+            object result = null;
+
+            try
+            {
+                ScriptSource s = engine.CreateScriptSourceFromString(line, SourceCodeKind.AutoDetect);
+                result = s.Execute(scope);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+  
+            if (result != null) 
+                Console.WriteLine("Result from IronRuby: " + result);
+            else
+                Console.WriteLine("Method returned nil");
+        }
+
+        private void SetConsoleColour()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+        }
+
+        private void ResetConsole()
+        {
+            Console.ResetColor();
+            Console.Clear();
+        }
+
+        private string GetLine(string lang)
+        {
+            Console.Write(lang + "> ");
+            string line = Console.ReadLine();
+            return line;
+        }
+    }
+}
---
+++ EmbeddedIronRuby/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmbeddedIronRuby
+{
+   class Program
+   {
+      [STAThread]
+      static void Main()
+      {
+         OutputHeader();
+         DlrHost host = new DlrHost();
+         host.Run();
+      }
+
+      private static void OutputHeader()
+      {
+         Console.WriteLine("Welcome to Embedded IronRuby Sample");
+         Console.WriteLine("Type ir to enter IronRuby");
+         Console.WriteLine("Type exit to quit the application");
+      }
+   }
+}
