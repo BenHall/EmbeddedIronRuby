82c275ad57eabcaa0c864cbb4702610130081f22
BenHall <ben@benhall.me.uk>
2010-06-03 00:19:30 +0100

    Being able to modify the world around it

---
+++ EmbeddedIronRuby/ConsoleWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EmbeddedIronRuby
+{
+    public class ConsoleWrapper
+    {
+        public ConsoleColor bg { set { Console.BackgroundColor = value; } }
+        public ConsoleColor fg { set { Console.ForegroundColor = value; } }
+        public string title { set { Console.Title = value; } }
+    }
+}
--- EmbeddedIronRuby/Program.cs
+++ EmbeddedIronRuby/Program.cs
@@ -1,22 +1,105 @@
 ﻿using System;
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting;
 
 namespace EmbeddedIronRuby
 {
-   class Program
-   {
-      [STAThread]
-      static void Main()
-      {
-         OutputHeader();
-         DlrHost host = new DlrHost();
-         host.Run();
-      }
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OutputHeader();
+            DLRHost host = new DLRHost();
+            host.Run();
+        }
 
-      private static void OutputHeader()
-      {
-         Console.WriteLine("Welcome to Embedded IronRuby Sample");
-         Console.WriteLine("Type ir to enter IronRuby");
-         Console.WriteLine("Type exit to quit the application");
-      }
-   }
+        private static void OutputHeader()
+        {
+            Console.WriteLine("Welcome to Embedded IronRuby Sample");
+            Console.WriteLine("Type ir to enter IronRuby");
+            Console.WriteLine("Type exit to quit the application");
+        }
+
+    }
+
+    public class DLRHost
+    {
+        ScriptEngine Engine;
+        ScriptScope Scope;
+        public DLRHost()
+        {
+            Engine = IronRuby.Ruby.CreateEngine();
+            Scope = Engine.CreateScope();
+            ExecuteWPF();
+        }
+
+        private void ExecuteWPF()
+        {
+            Execute("require 'mscorlib'");
+            Execute("require 'PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'");
+            Execute("require 'PresentationCore, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'");
+            
+            Execute("include System");
+            Execute("include System::Windows");
+            Execute("include System::Windows::Controls");
+        }
+
+        public void Run()
+        {
+            string line = GetLine("C#");
+
+            while (!line.Equals("exit") && !line.Equals(string.Empty))
+            {
+                if (line.Equals("ir"))
+                    EnterIronRuby();
+                else
+                    Console.WriteLine("You typed in: " + line);
+
+                line = GetLine("C#");
+            }
+        }
+
+        private string GetLine(string lang)
+        {
+            Console.Write(lang + "> ");
+            string line = Console.ReadLine();
+            return line;
+        }
+
+        private void EnterIronRuby()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+
+            Scope.SetVariable("c", new ConsoleWrapper());
+
+            string code = GetLine("ir");
+
+            while (!code.Equals("exit") && !code.Equals(string.Empty))
+            {
+                Execute(code);
+                code = GetLine("ir");
+            }
+
+            Console.ResetColor();
+            Console.Clear();
+        }
+
+        private void Execute(string code)
+        {
+            try
+            {
+                ScriptSource source = Engine.CreateScriptSourceFromString(code, SourceCodeKind.Statements);
+                object result = source.Execute(Scope);
+                if (result != null)
+                    Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+        }
+    }
 }
