11a9eb282a631e7e07527b00c47e572878d1c010
BenHall <ben@benhall.me.uk>
2010-06-11 01:03:21 +0100

    Start base

---
+++ EmbeddedIronRuby/UpdateUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmbeddedIronRuby
+{
+    public class UpdateUI
+    {
+        public delegate void DataChangedEventHandler(object sender, DataChangedEventArgs e);
+        public event DataChangedEventHandler DataChanged;
+
+        public UpdateUI()
+        {
+            DataChanged += Updated;
+        }
+
+        void Updated(object sender, DataChangedEventArgs e)
+        {
+            Console.WriteLine("I've been called (This is C#) " + e.DataFromRuby);
+        }
+
+        public virtual void Updated(string data)
+        {
+            if (DataChanged != null)
+            {
+                var args = new DataChangedEventArgs();
+                args.DataFromRuby = data;
+                DataChanged(this, args);
+            }
+        }
+    }
+
+    public class DataChangedEventArgs : EventArgs
+    {
+        public string DataFromRuby { get; set; }
+    }
+}
